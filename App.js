6d542b7cc3f101042ac6b26039de09e8aba1fec7
Jiwon <chloe326o.o@gmail.com>
2020-02-15 19:24:32 +0900

    Title & Card

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text, 
+  View, 
+  StatusBar, 
+  TextInput, 
+  Dimensions,
+  Platform
+ } from 'react-native';
+
+const {height, width} = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="dark-content"/>
+      <Text style={styles.title}>SPENT</Text>
+      <View style ={styles.card}>
+        <TextInput style={styles.input} placeholder={"Money"}/>
+      </View>
     </View>
   );
 }
@@ -14,6 +28,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title:{
+    color:"gray",
+    fontSize:30,
+    marginTop:50,
+    fontWeight: "400",
+    marginBottom:30
+  },
+  card : {
+    backgroundColor: "gray",
+    flex : 1,
+    width: width - 50,
+    ...Platform.select({
+      ios:{
+        shadowColor : "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset:{
+          height:-1,
+          width:0
+        }
+      },
+      android:{
+          elevation: 3
+      }
+    })
+  }
 });
